874cb3e40a864ed772fb4296d18cf3a74d0dedf7
Rob Bosman <rob.bosman.nl+github@gmail.com>
2018-01-27 23:02:36 +0100

    initial commit

---
+++ src/main/java/nl/bransom/coinr/Main.java
@@ -0,0 +1,13 @@
+package nl.bransom.coinr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+  public static void main(final String[] args) {
+    LOG.info("Hello CoinR!");
+  }
+}
